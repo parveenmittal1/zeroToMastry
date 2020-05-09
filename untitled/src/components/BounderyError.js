e4b518091fedde95398e985c4131c2df945266cd
parveen <mittalparveen652@gmail.com>
2020-05-09 11:21:44 +0530

    final complete

---
+++ untitled/src/components/BounderyError.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+class BounderyError extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false,
+        }
+    }
+
+    componentDidCatch(error,info) {
+        this.state({hasError:true})
+    }
+    render(){
+        if(this.state.hasError){
+            return <h1>Somthing is not available</h1>
+        }
+        return this.props.children;
+    }
+    }
+
+export default BounderyError
